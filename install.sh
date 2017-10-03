79b1611d480695bfa72832f2f1e9611a631fd079
kherio <jvrsanjose@gmail.com>
2017-10-03 09:53:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 function install-neutron-packages-controller() {
-	echo "Installing Neutron for Controller"
+	echo "Instalando NEUTRON en el Controller..."
 	sleep 2
 	apt-get install neutron-server neutron-plugin-ml2 \
   		neutron-linuxbridge-agent neutron-dhcp-agent \
@@ -8,13 +8,13 @@ function install-neutron-packages-controller() {
 }
 
 function install-cinder-packages-controller() {
-	echo "Installing Cinder for Controller"
+	echo "Instalando CINDER en el Controller..."
 	sleep 2
 	apt-get install cinder-api cinder-scheduler python-cinderclient -y
 }
 	
 function install-ceilometer-packages-controller() {
-	echo "Installing Ceilometer for Controller"
+	echo "Instalando Ceilometer en el Controller..."
 	sleep 2
 	apt-get install mongodb-server mongodb-clients python-pymongo -y
 	sleep 2
@@ -24,35 +24,35 @@ function install-ceilometer-packages-controller() {
 }
 
 function install-heat-packages-controller() {
-	echo "Installing Heat for Controller..."
+	echo "Instalando Heat en el Controller..."
 	sleep 2
 	apt-get install heat-api heat-api-cfn heat-engine \
   				python-heatclient -y
 }
 
 function install-common-packages() {
-	echo "About to install crudini"
+	echo "Instalando CRUDINI..."
 	apt-get install crudini -y
 	sleep 3
 
-	echo "About to install NTP Server"
+	echo "Instalando Servidor NTP..."
 	sleep 3
 	apt-get install chrony -y
 	service chrony restart
 	
-	echo "About to configure APT for Ocata"
+	echo "Añadiendo repositorio de PIKE..."
 	sleep 3
 	apt-get install software-properties-common -y
 	ubuntu_version=`lsb_release -sr`
 	if [ "$ubuntu_version" == "17.04" ] || [ "$ubuntu_version" == "16.04" ]
 	then
-		add-apt-repository cloud-archive:ocata
+		add-apt-repository cloud-archive:pike
 	else
-		echo "Ocata release supported only on Zesty (17.04) and Xenial (16.04)"
+		echo "Pike release supported only on Zesty (17.04) and Xenial (16.04)"
 		exit 1;
 	fi
 
-	echo "Doing full system update"
+	echo "update & upgrade & dist-upgrade del sistema..."
 	sleep 3
 	apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
 	apt-get autoremove -y
@@ -61,30 +61,30 @@ function install-common-packages() {
 
 
 function install-controller-packages() {
-	echo "Installing MariaDB and MongoDB..."
+	echo "Instalando MariaDB y MongoDB..."
 	apt-get install mariadb-server python-pymysql -y
 
-	echo "Installing RabbitMQ..." 
+	echo "Instalando RabbitMQ..." 
 	sleep 3
 	apt-get install rabbitmq-server -y
 	
-	echo "Installing Keystone..."
+	echo "Instalando Keystone..."
 	echo "manual" > /etc/init/keystone.override
 	sleep 3
 	apt-get install keystone apache2 libapache2-mod-wsgi memcached python-memcache -y
 	
-	echo "Installing Glance..."
+	echo "Instalando Glance..."
 	sleep 2
 	apt-get install glance python-glanceclient -y
 	
-	echo "Installing Nova for Controller"
+	echo "Instalando Nova en el Controller..."
 	sleep 2
 	apt-get install nova-api nova-cert nova-conductor nova-consoleauth nova-novncproxy \
 	nova-scheduler nova-placement-api python-novaclient -y
 
 	install-neutron-packages-controller
 	
-	echo "Installing Horizon..."
+	echo "Instalado Horizon..."
 	sleep 2
 	apt-get install openstack-dashboard -y
 	
@@ -104,7 +104,7 @@ function install-controller-packages() {
 }
 
 function install-networknode-packages() {
-	echo "About to install Neutron for Network Node..."
+	echo "Instalando servicios de Red..."
 	sleep 2
 	apt-get install neutron-plugin-ml2 neutron-plugin-linuxbridge-agent \
 	neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent python-neutronclient conntrack -y
@@ -112,15 +112,15 @@ function install-networknode-packages() {
 }
 
 function install-compute-packages() {
-	echo "About to install Nova for Compute"
+	echo "Instalando Nova en el Compute"
 	sleep 3
 	apt-get install nova-compute sysfsutils -y
 
-	echo "About to install Neutron for Compute"
+	echo "Instalando Neutron en el Compute"
 	sleep 2
 	apt-get install neutron-plugin-linuxbridge-agent conntrack -y
 	
-	echo "About to install Ceilometer for Compute"
+	echo "Instalando Ceilometer en el Compute"
 	sleep 2
 	apt-get install ceilometer-agent-compute -y
 	
@@ -135,7 +135,7 @@ fi
 
 if [ "$1" == "allinone" ]
 	then
-		echo "Installing packages for All-in-One"
+		echo "Seleccionado All-in-one. Instalando toda la paqueteria..."
 		sleep 5
 		install-common-packages
 		install-controller-packages
@@ -144,7 +144,7 @@ if [ "$1" == "allinone" ]
 elif [ "$1" == "controller" ] || [ "$1" == "compute" ] || [ "$1" == "networknode" ]
 	then
 		install-common-packages
-		echo "Installing packages for: "$1
+		echo "Instalando paquetería en: "$1
 		sleep 5
 		install-$1-packages
 else
@@ -153,8 +153,8 @@ else
 fi
 
 echo "********************************************"
-echo "NEXT STEPS:"
-echo "** Update lib/config-paramters.sh for Interface names"
-echo "** Run the below command on all nodes:"
-echo "            configure.sh <controller_ip_address>"
+echo "SIGUIENTES PASOS:"
+echo "** Actualiza lib/config-paramters.sh poniendo los interfaces correctos"
+echo "** Ejecuta el comando de abajo en TODOS los nodos:"
+echo "       ./configure.sh <controller_ip_address>"
 echo "********************************************"
